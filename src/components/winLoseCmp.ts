d0a59f11f0d1d0907718b30959d078c888738093
Arthur Carabott <arthur@arthurcarabott.com>
2021-12-28 23:35:52 -0800

    winlose cmp logic

--- src/components/winLoseCmp.ts
+++ src/components/winLoseCmp.ts
@@ -4,26 +4,41 @@ import { DB } from "../api";
 export const winLoseCmp = (db: DB) => {
   const state = db.deref();
 
-  return state.playState === "playing"
-    ? null
-    : div(
-        {
-          style: {
-            position: "absolute",
-            left: 0,
-            top: 0,
-            width: "100%",
-            height: "100%",
-            background:
-              state.playState === "win" ? "rgb(43, 212, 156, 0.5)" : "rgba(43, 156, 212, 0.5)",
-            "font-size": "10vh",
-            display: "flex",
-            "justify-content": "center",
-            "align-items": "center",
-          },
-        },
-        state.playState === "win"
-          ? "Winner, Winner, Chicken Dinner!"
-          : "It's Game Over man, it's just Game Over",
-      );
+  if (state.playState === "playing") {
+    return null;
+  }
+
+  const { background, message } = (() => {
+    if (state.playState === "win") {
+      return {
+        background: "rgb(43, 212, 156, 0.5)",
+        message: "Winner, Winner, Chicken Dinner!",
+      };
+    } else if (state.playState === "lose") {
+      return {
+        background: "rgba(43, 156, 212, 0.5)",
+        message: "It's game over man, it's just game over",
+      };
+    } else {
+      throw new TypeError("invalid play state");
+    }
+  })();
+
+  return div(
+    {
+      style: {
+        position: "absolute",
+        left: 0,
+        top: 0,
+        width: "100%",
+        height: "100%",
+        background,
+        "font-size": "10vh",
+        display: "flex",
+        "justify-content": "center",
+        "align-items": "center",
+      },
+    },
+    message,
+  );
 };
